5d2962fbe8d71e377efacbfce531e5c41db31707
EduardoWisch <eduardo.wisch@escola.pr.gov.br>
2024-07-17 17:06:49 -0300

    verificando url e não o texto

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -3,16 +3,17 @@ describe('Quando clicar no botão não tenho conta', () => {
         cy.visit('/')
         cy.contains('h1', 'Login')
         cy.get('p').contains('Não tenho conta').click()
-        cy.contains('h1', 'Inscreva-se')
+        cy.url().should('include', '/signup')
     });
     it('Quando clicar no botão de cadastro, deve voltar a aba de login', () => {
         cy.visit('/signup')
         cy.get('button').contains('Cadastrar').click()
-        cy.contains('h1', 'Login')
+        cy.url().should('include', '/')
     });
     it('Quando clicado no botão de login, deve ir para a página principal e verificar se o h1 tem a cor blueviolet', () => {
         cy.visit('/')
         cy.get('button').contains('Acessar').click()
+        cy.url().should('include', '/main')
         cy.contains('h1', 'Você entrou na página principal').should('have.css', 'color', 'rgb(138, 43, 226)')
     })
 })
